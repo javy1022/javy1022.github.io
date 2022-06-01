dbaff2aa86031d66535427992736ecc223dd2afb
Javy Wu <javywu1022@gmail.com>
2022-06-01 08:30:42 -0700

    api key access

--- HW6/debug.py
+++ HW6/debug.py
@@ -1,19 +1,11 @@
-
 import requests
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-url = 'https://api.artsy.net/api/tokens/xapp_token'
-x = requests.post(url)
-
-@app.route('/')    
-def test():
+   
+def main():
     url = 'https://api.artsy.net/api/tokens/xapp_token'
     api_authenticate = {'client_id': '5263ac83d3faa4643a80', 'client_secret': 'f510da3282c88d7acacb8a4c7a2e613a' }
     x = requests.post(url, api_authenticate)
-    return 'x'
-
+    print(x.text)
 
 if __name__ == "__main__":
-   app.run(host='127.0.0.1', port=8080, debug=True)
+   main()
    
