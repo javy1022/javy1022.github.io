3ddb545fff1a68f6e5d7a509fa7cc62635e8495a
Javy Wu <javywu1022@gmail.com>
2022-06-01 14:56:01 -0700

    parse token

--- HW6/debug.py
+++ HW6/debug.py
@@ -1,11 +1,13 @@
 import requests
+import json
    
 def main():
     url = 'https://api.artsy.net/api/tokens/xapp_token'
     api_authenticate = {'client_id': '5263ac83d3faa4643a80', 'client_secret': 'f510da3282c88d7acacb8a4c7a2e613a' }
     x = requests.post(url, api_authenticate)
-    print(x.text)
-
+    y = x.json()
+    print(y["token"])
+    
 if __name__ == "__main__":
    main()
    
--- HW6/main.py
+++ HW6/main.py
@@ -3,16 +3,12 @@ import requests
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-url = 'https://api.artsy.net/api/tokens/xapp_token'
-x = requests.post(url)
-
 @app.route('/')    
 def test():
-    url = 'https://api.artsy.net/api/tokens/xapp_token'
+    url = https://api.artsy.net/api/tokens/xapp_token
     api_authenticate = {'client_id': '5263ac83d3faa4643a80', 'client_secret': 'f510da3282c88d7acacb8a4c7a2e613a' }
     x = requests.post(url, api_authenticate)
     return 'x'
-
 
 if __name__ == "__main__":
    app.run(host='127.0.0.1', port=8080, debug=True)
