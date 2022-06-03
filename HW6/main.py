d57b9a06d3f707f19bc570ef0765300a93c30537
Javy Wu <javywu1022@gmail.com>
2022-06-03 05:21:29 -0700

    get parsed data from API locally

--- HW6/main.py
+++ HW6/main.py
@@ -4,12 +4,15 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')    
-def test():
-    url = https://api.artsy.net/api/tokens/xapp_token
+def authentication():
+    url = 'https://api.artsy.net/api/tokens/xapp_token'
     api_authenticate = {'client_id': '5263ac83d3faa4643a80', 'client_secret': 'f510da3282c88d7acacb8a4c7a2e613a' }
-    x = requests.post(url, api_authenticate)
-    return 'x'
-
+    data = requests.post(url, api_authenticate)
+    data_toJson = data.json()
+    token = (data_toJson["token"])
+    return token
+    
+    
 if __name__ == "__main__":
    app.run(host='127.0.0.1', port=8080, debug=True)
    
