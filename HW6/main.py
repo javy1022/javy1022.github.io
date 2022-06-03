334566aa6afd6f04b37f6ca334ce31b3ababe522
Javy Wu <javywu1022@gmail.com>
2022-06-03 14:14:41 -0700

    implement search bar and its css

--- HW6/main.py
+++ HW6/main.py
@@ -1,18 +1,14 @@
-
 import requests
-from flask import Flask, jsonify
-app = Flask(__name__)
+from flask import Flask, jsonify, redirect, url_for
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')    
-def authentication():
-    url = 'https://api.artsy.net/api/tokens/xapp_token'
-    api_authenticate = {'client_id': '5263ac83d3faa4643a80', 'client_secret': 'f510da3282c88d7acacb8a4c7a2e613a' }
-    data = requests.post(url, api_authenticate)
-    data_toJson = data.json()
-    token = (data_toJson["token"])
-    return token
-    
-    
+def home():
+    return redirect(url_for('static', filename='index.html'))   
+
+
+
+
 if __name__ == "__main__":
-   app.run(host='127.0.0.1', port=8080, debug=True)
+   app.run(debug=True)
    
