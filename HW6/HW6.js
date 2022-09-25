06e9384f1ee6148b5bdcc88e50ceb49a26ae7a8e
Javy Wu <javywu1022@gmail.com>
2022-09-24 22:28:49 -0700

    add javascript

---
+++ HW6/HW6.js
@@ -0,0 +1,7 @@
+function clear_fields() {
+	 document.getElementById("keyword").value = "";
+	 document.getElementById("location").value = "";
+	 document.getElementById("distance").value = "10";
+	 document.getElementById("category_bar").value = "Default";
+	 document.getElementById("check_box").checked = false;
+}
