acc5fa7c6d28485dae82f855d7969b9039c51cd9
Javy Wu <javywu1022@gmail.com>
2022-10-05 08:38:28 -0700

    successful ajax call

--- HW6/main.py
+++ HW6/main.py
@@ -1,14 +1,37 @@
-from flask import Flask, url_for, request, redirect
+import requests
+from flask import Flask, jsonify, redirect, url_for, json, request
+
 app = Flask(__name__)
 
 
-@app.route('/', methods =["GET", "POST"])
-def proxy():
-   
-    return "aloha"
+api_key = "P5t7BttvGrXWg8bb79sSf_HpQ_B-S2HShUbc8nwlp4I3DFaPKVpuXMuo3sIhELcn-xxnPpLuilHqnfaQLXwU_DZ4tALO0UZ0Wd9j3YLSEp89rb6SEUxwDFwckOgvY3Yx"
+API_HOST = "https://api.yelp.com/v3"
+BUSINESS_SEARCH_PATH = "/businesses/search" 
+
+headers = {
+'Authorization': 'Bearer %s' % api_key,
+}
+
+@app.route('/')
+def homepage():
+    return app.send_static_file("index.html")
 
 
-
+@app.route('/<string:keyword>/<string:location>/<string:category>/<int:distance>')
+def get_keyword(keyword,location,category,distance):
+    url =  API_HOST + BUSINESS_SEARCH_PATH
+    headers = {'Authorization': 'Bearer %s' % api_key,}
+    
+    url_params = {	'term': keyword,
+					'location': location,	
+					'category': category,				
+					'radius': distance
+					}
+    response = requests.get(url, headers=headers, params=url_params)
+    response_json = response.json()
+    response_dump = json.dumps(response_json)
+    
+    return jsonify(response_dump) 
 
 if __name__ == "__main__":
     app.run()
--- HW6/static/index.js
+++ HW6/static/index.js
@@ -6,27 +6,59 @@
 
 function clear_fields() {
 	 document.getElementById("keyword").value = "";
-	 document.getElementById("location").value = "";
+	 document.getElementById("locations").value = "";
 	 document.getElementById("distance").value = "10";
 	 document.getElementById("category_bar").value = "Default";
 	 document.getElementById("check_box").checked = false;
 	
 }
 
+function send_request(url) {
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      resp = this.responseText;
+	  alert(resp);
+    }
+ };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+//https://api.yelp.com/v3/businesses/search?term=Sushi&latitude=33.8491816&longitude=-118.3884078&categories=Food&radius=5
+function get_yelp_result(){
+	var form_keyword = form_obj.get('keywords');
+	var form_location= form_obj.get('locations');
+	var form_category= form_obj.get('category');
+		
+	send_request("/" + form_keyword + "/" + form_location + "/" +  form_category + "/" + "16093");
+	//alert("/?keywords=" + form_keyword + "&location=" + form_location  + "&category=" + form_category + "&distance=16093")
+}
+
+
 const submit_button = document.getElementById("submit");
 const keyword = document.getElementById('keyword');
 const locations = document.getElementById('locations');
 const form_data = document.getElementById('form_data');
-submit_button.addEventListener("click", checkboxClick, false);
+submit_button.addEventListener("click", submitForm, false);
 
 let form_obj = new FormData(form_data);
 
-function checkboxClick(event) {
+function submitForm(event) {
+		
  if(keyword.checkValidity() != false && locations.checkValidity() != false ){
-	 event.preventDefault();
-	 alert(form_obj.get('keywords'));
-	
-	  
- } 
+	event.preventDefault();
+	get_yelp_result();
+	/*var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+		}
+  };
+    xhttp.open("GET", "/", true);
+    xhttp.send();
+ }*/
 }
-
+}
