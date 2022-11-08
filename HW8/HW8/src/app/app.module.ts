8ad97a7cfd6bdee8fb32201397296c4cdbe24c14
Javy Wu <javywu1022@gmail.com>
2022-11-08 06:35:33 -0800

    use a new cloned repo because previoous one is broken

--- HW8/HW8/src/app/app.module.ts
+++ HW8/HW8/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,8 @@ import { MyBookingsComponent } from './my-bookings/my-bookings.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
