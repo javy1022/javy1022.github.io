409c041b6fadc189c5c73765e341079a3681471f
javy1022 <80365661+javy1022@users.noreply.github.com>
2023-01-15 04:25:13 -0800

    Delete HW6 directory

--- HW6/HW6.js
+++
@@ -1,7 +0,0 @@
-function clear_fields() {
-	 document.getElementById("keyword").value = "";
-	 document.getElementById("location").value = "";
-	 document.getElementById("distance").value = "10";
-	 document.getElementById("category_bar").value = "Default";
-	 document.getElementById("check_box").checked = false;
-}
