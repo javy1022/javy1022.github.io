1f967175fc2304af446d8585c86555cdeeb50dae
javy1022 <javywu1022@gmail.com>
2023-02-11 02:40:48 -0800

    Added Clear and Checkbox buttons' functionality

---
+++ HW6/static/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+
+const location_form = document.getElementById("locations");
+
+function clear_fields() {
+  document.getElementById("keyword").value = "";
+  document.getElementById("distance").value = "";
+  document.getElementById("category_bar").value = "Default";
+  document.getElementById("check_box").checked = false;
+
+  if (location_form.style.display == "none") {
+    location_form.style.display = "initial";
+    location_form.required = true;
+  } else {
+    document.getElementById("locations").value = "";
+  }
+
+  //document.getElementById("table").innerHTML = "";
+  //document.getElementById("card_holder").innerHTML = "";
+  //document.getElementById("no_record_container").innerHTML = "";
+
+  //list_for_table = [];
+}
+
+function check(event) {
+  if (location_form.style.display == "none") {
+    location_form.style.display = "initial";
+    location_form.required = true;
+  } else {
+    location_form.value = "";
+    location_form.required = false;
+    location_form.style.display = "none";
+  }
+}
+
