f2560c601d3c8791c4ab36e37dd108028e52bac1
javy1022 <javywu1022@gmail.com>
2023-02-14 00:07:11 -0800

    get lat and long form Google Geocoding api successfully

--- HW6/static/index.js
+++ HW6/static/index.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-unused-vars */
 
+const GOOGLE_API_HOST = "https://maps.googleapis.com";
+const GEOCODING_SEARCH_PATH = "/maps/api/geocode/json";
+const GOOGLE_API_KEY = "AIzaSyDIGpZqNauMP2qqXnnanXzP6ba0AIIkT58";
 const reg_non_alphanumeric  = /[^a-z0-9+]+/gi;
 const reg_remove_all_spaces_after_end_string  = /\s*$/;
+
 
 const location_form = document.getElementById("locations");
 const keyword = document.getElementById('keyword');
@@ -46,8 +51,13 @@ function submitForm(event) {
 	
   if(keyword.checkValidity() != false && location_form.checkValidity() != false && location_form.style.display != "none"){
    event.preventDefault();
-   console.log("ok")
    
+   var form_location= location_form.value;
+   var buffer = form_location.replace(reg_remove_all_spaces_after_end_string, "");
+   var api_address_param = buffer.replace(reg_non_alphanumeric, '+');
+   var url = GOOGLE_API_HOST + GEOCODING_SEARCH_PATH + "?address=" + api_address_param + "&key=" + GOOGLE_API_KEY;
+
+   geoCode_send_request(url);
 
  }else{
   console.log("ohmygod")
@@ -55,3 +65,21 @@ function submitForm(event) {
  }
 }
 
+
+function geoCode_send_request(url) {
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("bingo")
+	    var resp = this.responseText;
+	    var result_dict = JSON.parse(resp) ;
+	    
+      if(result_dict["status"] != "ZERO_RESULTS"){
+        //get_yelp_result(result_dict["results"]["0"]["geometry"]["location"]["lat"], result_dict["results"]["0"]["geometry"]["location"]["lng"]);
+      }
+	 }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
