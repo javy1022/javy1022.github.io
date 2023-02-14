be5dc2948846508ab50b53e8b83732265b38d1d3
javy1022 <javywu1022@gmail.com>
2023-02-13 23:33:16 -0800

    prevented default submit

--- HW6/static/index.js
+++ HW6/static/index.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-unused-vars */
 
+const reg_non_alphanumeric  = /[^a-z0-9+]+/gi;
+const reg_remove_all_spaces_after_end_string  = /\s*$/;
+
 const location_form = document.getElementById("locations");
+const keyword = document.getElementById('keyword');
+
+const form_data = document.getElementById('form_data');
+const submit_button = document.getElementById("submit");
+
 
 function clear_fields() {
   document.getElementById("keyword").value = "";
@@ -31,5 +39,19 @@ function check(event) {
     location_form.required = false;
     location_form.style.display = "none";
   }
+}
+
+submit_button.addEventListener("click", submitForm, false);
+function submitForm(event) {
+	
+  if(keyword.checkValidity() != false && location_form.checkValidity() != false && location_form.style.display != "none"){
+   event.preventDefault();
+   console.log("ok")
+   
+
+ }else{
+  console.log("ohmygod")
+
+ }
 }
 
