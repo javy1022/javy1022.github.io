2f9e4d319d776422d00922b691cb73e179766d57
javy1022 <javywu1022@gmail.com>
2023-02-14 05:48:07 -0800

    Inserted fields to table correctly

--- HW6/static/index.js
+++ HW6/static/index.js
@@ -12,9 +12,11 @@ const keyword = document.getElementById("keyword");
 const distance = document.getElementById("distance");
 const category = document.getElementById("category_bar");
 const location_form = document.getElementById("locations");
-
 const form_data = document.getElementById("form_data");
 const submit_button = document.getElementById("submit");
+
+const item_table =  document.getElementById("table");
+
 
 function clear_fields() {
   document.getElementById("keyword").value = "";
@@ -49,26 +51,13 @@ function check(event) {
 
 submit_button.addEventListener("click", submitForm, false);
 function submitForm(event) {
-  if (
-    keyword.checkValidity() != false &&
-    location_form.checkValidity() != false &&
-    location_form.style.display != "none"
-  ) {
+  if (keyword.checkValidity() != false && location_form.checkValidity() != false && location_form.style.display != "none") {
     event.preventDefault();
 
     let form_location = location_form.value;
-    let buffer = form_location.replace(
-      reg_remove_all_spaces_after_end_string,
-      ""
-    );
+    let buffer = form_location.replace(reg_remove_all_spaces_after_end_string, "");
     let api_address_param = buffer.replace(reg_non_alphanumeric, "+");
-    let url =
-      GOOGLE_API_HOST +
-      GEOCODING_SEARCH_PATH +
-      "?address=" +
-      api_address_param +
-      "&key=" +
-      GOOGLE_API_KEY;
+    let url = GOOGLE_API_HOST + GEOCODING_SEARCH_PATH + "?address=" + api_address_param + "&key=" + GOOGLE_API_KEY;
 
     geoCode_send_request(url);
   } else {
@@ -86,10 +75,7 @@ function geoCode_send_request(url) {
       let result_dict = JSON.parse(resp);
 
       if (result_dict["status"] != "ZERO_RESULTS") {
-        get_yelp_result(
-          result_dict["results"]["0"]["geometry"]["location"]["lat"],
-          result_dict["results"]["0"]["geometry"]["location"]["lng"]
-        );
+        get_yelp_result(result_dict["results"]["0"]["geometry"]["location"]["lat"], result_dict["results"]["0"]["geometry"]["location"]["lng"]);
       }
     }
   };
@@ -106,31 +92,9 @@ function get_yelp_result(lat, lng) {
   else form_distance = distance.value;
 
   if (typeof lat == "number" && typeof lng == "number") {
-    send_request(
-      "/" +
-        form_keyword +
-        "/" +
-        lat.toString() +
-        "/" +
-        lng.toString() +
-        "/" +
-        form_category +
-        "/" +
-        form_distance
-    );
+    send_request("/" + form_keyword + "/" + lat.toString() + "/" + lng.toString() + "/" + form_category + "/" + form_distance);
   } else if (typeof lat == "string" && typeof lng == "string") {
-    send_request(
-      "/" +
-        form_keyword +
-        "/" +
-        lat +
-        "/" +
-        lng +
-        "/" +
-        form_category +
-        "/" +
-        form_distance
-    );
+    send_request("/" + form_keyword + "/" + lat + "/" + lng + "/" + form_category + "/" + form_distance);
   }
 }
 
@@ -143,29 +107,42 @@ function send_request(url) {
     if (this.readyState == 4 && this.status == 200) {
       let resp = this.responseText;
       let result_dict = JSON.parse(resp);
-      //console.log(result_dict)
-      console.log(result_dict["_embedded"]["events"][0]);
 
       let total_events = Object.keys(result_dict["_embedded"]["events"]).length;
 
       for (let i = 0; i < total_events; i++) {
-        let result_dict_item = Object.entries(
-          result_dict["_embedded"]["events"][i]
-        );
+        let result_dict_item = Object.entries(result_dict["_embedded"]["events"][i]);
         let buffer_array = new Array();
 
         buffer_array_append(result_dict_item, buffer_array, "dates");
         buffer_array_append(result_dict_item, buffer_array, "images");
+        buffer_array_append(result_dict_item, buffer_array, "name");
         buffer_array_append(result_dict_item, buffer_array, "classifications");
         buffer_array_append(result_dict_item, buffer_array, "_embedded");
-       /*
-     	
-      
+
         list_for_table.push(buffer_array);
-        */
-        // console.log(result_dict_item)
-        console.log(buffer_array)
+       
       }
+      console.log(list_for_table)
+
+
+      if(list_for_table.length > 0){
+        table_header_constructor(item_table);
+      }else{
+         
+       console.log("no record")
+       
+      } 
+      
+      for (let i = 0; i < total_events  ; i++) {
+        table_append_row(item_table, list_for_table, i);
+        } 
+
+
+
+
+
+
     }
   };
   xhttp.open("GET", url, true);
@@ -175,36 +152,49 @@ function send_request(url) {
 function buffer_array_append(result_dict_item, buffer_array, header) {
   for (let i = 0; i < Object.keys(result_dict_item).length; i++) {
     if (result_dict_item[i][0] == header) {
-        if(header == "dates"){
-            let time_obj = result_dict_item[i][1]["start"]
-            if("localDate" in time_obj) buffer_array.push(time_obj["localDate"]);
-            else buffer_array.push(" ")
+      if (header == "dates") {
+        let time_obj = result_dict_item[i][1]["start"];
+        if ("localDate" in time_obj) buffer_array.push(time_obj["localDate"]);
+        else buffer_array.push(" ");
 
-            if("localTime" in time_obj) buffer_array.push(time_obj["localTime"]);
-            else buffer_array.push(" ")
-        }
-        else if (header == "images"){
-            let image_obj = result_dict_item[i][1]
-            if(image_obj.length != 0) buffer_array.push(image_obj[0]["url"]);
-            else buffer_array.push(" ")
-        }
-        else if (header == "name"){
-            let name_obj = result_dict_item[i][1]
-            if(name_obj.length != 0) buffer_array.push(name_obj);
-            else buffer_array.push(" ")
-        }
-        else if (header == "classifications"){
-          let genre_obj = result_dict_item[i][1]
-          
-          if("name" in genre_obj[0]["segment"]) buffer_array.push(genre_obj[0]["segment"]["name"]);
-          else  buffer_array.push(" ")
-        }
-        else if (header == "_embedded") {
-          let venues_obj = result_dict_item[i][1]["venues"][0]   
+        if ("localTime" in time_obj) buffer_array.push(time_obj["localTime"]);
+        else buffer_array.push(" ");
+      } else if (header == "images") {
+        let image_obj = result_dict_item[i][1];
+        if (image_obj.length != 0) buffer_array.push(image_obj[0]["url"]);
+        else buffer_array.push(" ");
+      } else if (header == "name") {
+        let name_obj = result_dict_item[i][1];
+        if (name_obj.length != 0) buffer_array.push(name_obj);
+        else buffer_array.push(" ");
+      } else if (header == "classifications") {
+        let genre_obj = result_dict_item[i][1];
 
-          if(venues_obj["name"].length != 0 ) buffer_array.push(venues_obj["name"]);
-          else buffer_array.push(" ")
-        }
+        if ("name" in genre_obj[0]["segment"]) buffer_array.push(genre_obj[0]["segment"]["name"]);
+        else buffer_array.push(" ");
+      } else if (header == "_embedded") {
+        let venues_obj = result_dict_item[i][1]["venues"][0];
+
+        if (venues_obj["name"].length != 0) buffer_array.push(venues_obj["name"]);
+        else buffer_array.push(" ");
+      }
     }
   }
+}
+
+function table_header_constructor(item_table){
+	item_table.innerHTML += "<tr id =\"first_row_height\"><th id =\"first_columns_width\">Date</th> <th id =\"second_columns_width\">Icon</th> <th id =\"third_columns_width\" onClick=\"sort_table(this.id)\" >Event</th> <th id =\"fourth_columns_width\" onClick=\"sort_table(this.id)\">Genre</th> <th id =\"fifth_columns_width\" onClick=\"sort_table(this.id)\">Venue</th>  </tr>";
+	document.getElementById("third_columns_width").style.cursor = "pointer";
+	document.getElementById("fourth_columns_width").style.cursor = "pointer";
+	document.getElementById("fifth_columns_width").style.cursor = "pointer";
+}
+
+function table_append_row(item_table, list_for_table, i){
+		
+	item_table.innerHTML += "<tr class=\"rows_height\"><td class=\"table_text\">" +  list_for_table[i][0] + "</td><td><img src=" + list_for_table[i][2] + " class=\"yelp_image\"></img></td> <td class=\"table_text\"> <a href = \"#\" class=\"name_title\"  onclick='business_detail_request(\""+ list_for_table[i][4] +"\");' >" +  list_for_table[i][3]  + "</a></td> <td class=\"table_text\">" + list_for_table[i][4] + "</td> <td class=\"table_text\">" + list_for_table[i][5] +"</td> </tr>";
+	
+}
+
+function sort_table(id){
+	console.log("sort placeholder")
 }
