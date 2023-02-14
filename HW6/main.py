26bbea7a9e93f373157c927a7b47a29f2aaafb83
javy1022 <javywu1022@gmail.com>
2023-02-14 01:19:39 -0800

    send request to Flask backend with needed vairables

--- HW6/main.py
+++ HW6/main.py
@@ -1,6 +1,14 @@
 import requests
 from flask import Flask, jsonify, redirect, url_for, json, request, send_from_directory
 
+TICKETMASTER_API_KET = "PbITZwAJ32tYq1Codl5AhoRCENI3fPfo"
+
+MUSIC_SEGMENT_ID = "KZFzniwnSyZfZ7v7nJ"
+SPORTS_SEGMENT_ID  = "KZFzniwnSyZfZ7v7nE"
+ARTS_THEATRE_SEGMENT_ID   = "KZFzniwnSyZfZ7v7na"
+FILM_SEGMENT_ID  = "KZFzniwnSyZfZ7v7nn"
+MISCELLANEOUS_SEGMENT_ID = "KZFzniwnSyZfZ7v7n1"
+DISTANCE_UNIT = "miles"
 
 app = Flask(__name__)
 
@@ -10,6 +18,9 @@ app = Flask(__name__)
 def homepage():
     return app.send_static_file("index.html")
 
+@app.route('/<string:keyword>/<string:lat_string>/<string:lng_string>/<string:category>/<int:distance>')
+def get_search_result(keyword,lat_string,lng_string,category,distance):
+    return str(distance)
 
 if __name__ == "__main__":
     app.run()
--- HW6/static/index.js
+++ HW6/static/index.js
@@ -6,13 +6,16 @@ const GEOCODING_SEARCH_PATH = "/maps/api/geocode/json";
 const GOOGLE_API_KEY = "AIzaSyDIGpZqNauMP2qqXnnanXzP6ba0AIIkT58";
 const reg_non_alphanumeric  = /[^a-z0-9+]+/gi;
 const reg_remove_all_spaces_after_end_string  = /\s*$/;
+const DEFAULT_DISTANCE = "10"
 
 
+const keyword = document.getElementById("keyword");
+const distance = document.getElementById("distance")
+const category= document.getElementById("category_bar");
 const location_form = document.getElementById("locations");
-const keyword = document.getElementById('keyword');
 
 const form_data = document.getElementById('form_data');
-const submit_button = document.getElementById("submit");
+const submit_button = document.getElementById('submit');
 
 
 function clear_fields() {
@@ -76,10 +79,46 @@ function geoCode_send_request(url) {
 	    var result_dict = JSON.parse(resp) ;
 	    
       if(result_dict["status"] != "ZERO_RESULTS"){
-        //get_yelp_result(result_dict["results"]["0"]["geometry"]["location"]["lat"], result_dict["results"]["0"]["geometry"]["location"]["lng"]);
+        get_yelp_result(result_dict["results"]["0"]["geometry"]["location"]["lat"], result_dict["results"]["0"]["geometry"]["location"]["lng"]);
       }
 	 }
   };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+
+function get_yelp_result(lat, lng){
+	var form_keyword = keyword.value;
+	var form_category= category.value;
+  var form_distance;
+	
+  if(distance.value == "") form_distance = DEFAULT_DISTANCE;
+  else form_distance = distance.value;
+
+	if((typeof lat) == "number" && (typeof lng) == "number"){
+		send_request("/" + form_keyword +  "/" + lat.toString() + "/" + lng.toString() + "/" +   form_category + "/" + form_distance);
+	
+	}
+	else if ((typeof lat) == "string" && (typeof lng) == "string") {
+		send_request("/" + form_keyword +  "/" + lat + "/" + lng + "/" +  form_category + "/" + form_distance);
+		
+	}
+	
+}
+
+function send_request(url) {
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+	    var resp = this.responseText;
+	    //var result_dict = JSON.parse(resp) ;
+	    //console.log(result_dict);
+	  	console.log(resp)
+    }
+	
+ };
   xhttp.open("GET", url, true);
   xhttp.send();
 }
