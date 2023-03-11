ec8acb55e8a698d0cd1e070a0e452bbc4ab199c0
javy1022 <javywu1022@gmail.com>
2023-03-10 23:48:31 -0800

    rename root folder and upload hw8_description

---
+++ HW8/back_end/routes/index.js
@@ -0,0 +1,71 @@
+/*const yelp_api_key = "uSjjTUgg-0j-e57O8dY9aDufA5u1HuNrvZ3tGIz-X5mAm98dYvR25gn6YVslv75zV3V9cwaEruFCuXiLar1E9dJq2TnWhc1JhCJJsoz7GMMMT2IIfdu_FEssjbnDY3Yx"
+const YELP_API_HOST = "https://api.yelp.com/v3"
+const BUSINESS_SEARCH_PATH = "/businesses/search" */
+
+const TICKETMASTER_API_KEY = "PbITZwAJ32tYq1Codl5AhoRCENI3fPfo"
+const TICKETMASTER_HOST = "https://app.ticketmaster.com"
+const EVENT_SEARCH_PATH = "/discovery/v2/events.json"
+const EVENT_DETAIL_PATH = "/discovery/v2/events/"
+const VENUE_DETAIL_PATH = "/discovery/v2/venues"
+
+const MUSIC_SEGMENT_ID = "KZFzniwnSyZfZ7v7nJ"
+const SPORTS_SEGMENT_ID  = "KZFzniwnSyZfZ7v7nE"
+const ARTS_THEATRE_SEGMENT_ID   = "KZFzniwnSyZfZ7v7na"
+const FILM_SEGMENT_ID  = "KZFzniwnSyZfZ7v7nn"
+const MISCELLANEOUS_SEGMENT_ID = "KZFzniwnSyZfZ7v7n1"
+const DISTANCE_UNIT = "miles"
+const GEOHASH_PRECISION = 7
+
+
+var express = require('express');
+var axios = require('axios');
+var router = express.Router();
+var geohash = require('ngeohash');
+
+router.get('/search', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');   
+  let gh = geohash.encode(req.query.lat, req.query.lng, GEOHASH_PRECISION);
+
+  const url_params = {
+	apikey: TICKETMASTER_API_KEY,
+	keyword: req.query.keyword,
+	segmentId: "",
+	radius: req.query.distance,
+	unit: DISTANCE_UNIT,
+	geoPoint: gh
+  }
+
+  if (req.query.category == "Music") {
+	url_params["segmentId"] = MUSIC_SEGMENT_ID;
+	} else if (req.query.category == "Sports") {
+	url_params["segmentId"] = SPORTS_SEGMENT_ID;
+	} else if (req.query.category == "Arts & Theatre") {
+	url_params["segmentId"] = ARTS_THEATRE_SEGMENT_ID;
+	} else if (req.query.category == "Film") {
+	url_params["segmentId"] = FILM_SEGMENT_ID;
+	} else if (req.query.category == "Miscellaneous") {
+	url_params["segmentId"] = MISCELLANEOUS_SEGMENT_ID;
+	}
+
+
+  const instance = axios.create({
+	baseURL: TICKETMASTER_HOST,
+	timeout: 1000
+  });
+
+
+ instance.get(EVENT_SEARCH_PATH, {params: url_params} )
+	.then(function (response) {
+		//console.log("Ok");
+		//console.log(response.data);
+		//return res.send(response.data);
+		return res.send(response.data);
+	})
+	.catch(function (error) {
+		//console.log("Error");
+	    //console.log(error.response.data);
+		return res.send(error.response.data);
+	});
+});
+
+module.exports = router;
