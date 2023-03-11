f5ce35f77a847843db01f31978357dad909e4ce7
javy1022 <javywu1022@gmail.com>
2023-03-10 18:38:52 -0800

    Moved router-outlet to the correct place, finish router buttons css

--- HW8_test/src/app/app.module.ts
+++ HW8_test/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FavoritesComponent } from './favorites/favorites.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFormComponent,
-    FavoritesComponent
+    FavoritesComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
