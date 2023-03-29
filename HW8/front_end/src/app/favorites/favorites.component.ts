3526a45f8864260f63ee1f1ad20b393380b44f21
javy1022 <javywu1022@gmail.com>
2023-03-29 13:26:29 -0700

    Add no favorite prompt and handle its logic

--- HW8/front_end/src/app/favorites/favorites.component.ts
+++ HW8/front_end/src/app/favorites/favorites.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit {
+   window: Window = window;
 
+  ngOnInit() {
+    this.window.localStorage.clear();
+
+    
+    alert(this.window.localStorage.length)
+  }
 }
