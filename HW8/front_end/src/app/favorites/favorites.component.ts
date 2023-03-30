24dd51100769999f80090a12bdb0fc3371ab0c2a
javy1022 <javywu1022@gmail.com>
2023-03-30 11:16:05 -0700

    minor cleanup

--- HW8/front_end/src/app/favorites/favorites.component.ts
+++ HW8/front_end/src/app/favorites/favorites.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { SharedService } from "../shared.service";
 
 
@@ -7,13 +7,10 @@ import { SharedService } from "../shared.service";
   templateUrl: "./favorites.component.html",
   styleUrls: ["./favorites.component.css"],
 })
-export class FavoritesComponent implements OnInit {
+export class FavoritesComponent {
   constructor(public sharedService: SharedService) {}   
 
-  ngOnInit(): void {
-    this.sharedService.window.localStorage.clear();
-    console.log("temp");
-  }
+
 
   remove_event_touch(event_index:number ,fav_icon_key:string ,event:  TouchEvent){
     let fav_table = this.sharedService.fav_storage_table; 
@@ -25,12 +22,13 @@ export class FavoritesComponent implements OnInit {
     localStorage.setItem("fav_toggles_dict", JSON.stringify(this.sharedService.fav_toggles_dict));
   }
 
-  remove_event_click(event_index:number, fav_icon_id:string,event:  MouseEvent){      
+  remove_event_click(event_index:number, fav_icon_key:string,event:  MouseEvent){      
     let fav_table = this.sharedService.fav_storage_table; 
     let local_storage = this.sharedService.window.localStorage;  
     fav_table.splice(event_index, 1);
     local_storage.setItem("fav", JSON.stringify(fav_table));
-    console.log(fav_icon_id)
+    this.sharedService.fav_toggles_dict[fav_icon_key] = !this.sharedService.fav_toggles_dict[fav_icon_key];
+    localStorage.setItem("fav_toggles_dict", JSON.stringify(this.sharedService.fav_toggles_dict));
   }
   
 }
