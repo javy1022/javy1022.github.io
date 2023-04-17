147cb2f7c5606a3f7cb6eb46cf10543b14885b77
javy1022 <javywu1022@gmail.com>
2023-04-17 03:51:12 -0700

    Add green back btn and implements its functionality

--- HW9/app/src/main/java/com/example/hw9/EventDetailsActivity.java
+++ HW9/app/src/main/java/com/example/hw9/EventDetailsActivity.java
@@ -1,16 +1,14 @@
 package com.example.hw9;
 
 import android.os.Bundle;
-
-import com.example.hw9.databinding.ActivityEventDetailsBinding;
-import com.example.hw9.ui.main.EventDetailsActivity.adapter.EventDetailsSectionsPagerAdapter;
-
-import com.google.android.material.tabs.TabLayout;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
-
+import com.example.hw9.databinding.ActivityEventDetailsBinding;
+import com.example.hw9.ui.main.EventDetailsActivity.adapter.EventDetailsSectionsPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class EventDetailsActivity extends AppCompatActivity {
@@ -29,5 +27,14 @@ public class EventDetailsActivity extends AppCompatActivity {
         TabLayout tabs = binding.tabs;
         TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabs, viewPager, (tab, position) -> tab.setText(sectionsPagerAdapter.getPageTitle(position)));
         tabLayoutMediator.attach();
+
+        // navigate back to previous activity
+        back_btn_onClick();
     }
+
+    private void back_btn_onClick(){
+        ImageButton backButton = findViewById(R.id.green_back_btn);
+        backButton.setOnClickListener(v -> onBackPressed());
+    }
+
 }
