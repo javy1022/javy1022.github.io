bd7e9483ede51e60cf05c8222858efd42d2a6cef
javy1022 <javywu1022@gmail.com>
2023-04-28 01:35:44 -0700

    finish venue card text view

--- HW9/app/src/main/java/com/example/hw9/ui/main/EventDetailsActivity/tabs/VenueFragment.java
+++ HW9/app/src/main/java/com/example/hw9/ui/main/EventDetailsActivity/tabs/VenueFragment.java
@@ -76,13 +76,12 @@ public class VenueFragment  extends Fragment {
     private void  get_and_utilize_venue_name(View view){
         getParentFragmentManager().setFragmentResultListener("venue_name", this, (requestKey, bundle) -> {
             String venue = bundle.getString("venue_name");
-
             // REST
-            get_venue_info(venue);
+            get_venue_info(view,venue);
         });
     }
 
-    private void get_venue_info(String venue){
+    private void get_venue_info(View view, String venue){
         String base_url = "https://csci571-hw8-spr23.wl.r.appspot.com/search/venue-detail";
         Uri.Builder builder = Uri.parse(base_url).buildUpon();
         builder.appendQueryParameter("venue_name", venue);
@@ -93,12 +92,77 @@ public class VenueFragment  extends Fragment {
                     Gson gson = new Gson();
                     JsonObject gson_resp = gson.fromJson(resp.toString(), JsonObject.class);
 
-                    Log.d("v request", "test resp: " + gson_resp);
+                    JsonArray desired_venue = shared.general_json_arr_navigator(gson_resp,"_embedded","venues");
+                    if (desired_venue!= null && desired_venue.size() > 0) {
+                        JsonObject venue_obj = desired_venue.get(0).getAsJsonObject();
+
+                        String venue_name = shared.general_json_navigator(venue_obj,"name");
+                        String address = shared.general_json_navigator(venue_obj,"address", "line1");
+                        String city = shared.general_json_navigator(venue_obj,"city", "name");
+                        String state = shared.general_json_navigator(venue_obj,"state", "name");
+                        String contact = shared.general_json_navigator(venue_obj,"boxOfficeInfo", "phoneNumberDetail");
+                        String hours = shared.general_json_navigator(venue_obj,"boxOfficeInfo", "openHoursDetail");
+                        String general_rule = shared.general_json_navigator(venue_obj,"generalInfo", "generalRule");
+                        String child_rule = shared.general_json_navigator(venue_obj,"generalInfo", "childRule");
+
+                        String city_state = custom_city_state_formatter(city,state);
+                        set_card_UI(view,venue_name, address, city_state, contact);
+
+                        Log.d("v request", "test resp: " + venue_name );
+                        Log.d("v request", "test resp: " + address );
+                        Log.d("v request", "test resp: " + city );
+                        Log.d("v request", "test resp: " + state );
+                        Log.d("v request", "test resp: " + contact );
+                        Log.d("v request", "test resp: " + hours );
+                        Log.d("v request", "test resp: " + general_rule);
+                        Log.d("v request", "test resp: " + child_rule);
+                        Log.d("v request", "test resp: " + city_state );
+
+                    }
 
                 }, error -> {
                     Log.e("Error", "Volley Error Get Venue Details: " + error.getMessage());
                 });
         MySingleton.getInstance(requireContext()).addToRequestQueue(jsonObjectRequest);
     }
+
+    private void set_card_UI(View view,String venue_name, String address, String city_state, String contact){
+        TextView venue_tv =  view.findViewById(R.id.venue);
+        TextView address_tv =  view.findViewById(R.id.address);
+        TextView city_tv =  view.findViewById(R.id.city);
+        TextView contact_tv =  view.findViewById(R.id.contact);
+
+        if(!venue_name.isEmpty()) venue_tv.setText(venue_name);
+        else venue_tv.setText("N/A");
+
+        if(!address.isEmpty()) address_tv.setText(address);
+        else address_tv.setText("N/A");
+
+        if(!city_state.isEmpty()) city_tv.setText(city_state);
+        else city_tv.setText("N/A");
+
+        if(!contact.isEmpty()) contact_tv.setText(contact);
+        else contact_tv.setText("N/A");
+
+        shared.textViews_enable_selected(venue_tv, address_tv, city_tv, contact_tv);
+    }
+
+    private String custom_city_state_formatter(String city, String state){
+        StringBuilder full_address_buffer = new StringBuilder();
+
+        if (!city.isEmpty()) {
+            full_address_buffer.append(city);
+        }
+
+        if (!state.isEmpty()) {
+            if (full_address_buffer.length() > 0) {
+                full_address_buffer.append(", ");
+            }
+            full_address_buffer.append(state);
+        }
+
+        return full_address_buffer.toString();
+    }
+
 
 }
